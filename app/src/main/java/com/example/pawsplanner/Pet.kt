3138d3e9f19b82b2caced704bc1466bed5328581
Juan Kenichi Sutan <kenichi.jks@gmail.com>
2023-03-04 23:29:35 +0900

    Create pet.kt class

---
+++ app/src/main/java/com/example/pawsplanner/Pet.kt
@@ -0,0 +1,13 @@
+package com.example.pawsplanner
+
+class Pet {
+    var name: String = ""
+    var type: String = ""
+    var age: String = ""
+
+    constructor(name: String, type: String, age: String) {
+        this.name = name
+        this.type = type
+        this.age = age
+    }
+}
